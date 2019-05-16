29836a3f370019a9a75703f35982dc09b5abda83
FerreWagner <18408229270@163.com>
2019-05-16 23:23:51 +0800

    add douyu file

--- bs4test1.py
+++ bs4test1.py
@@ -1,3 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+# res = requests.get("http://heater.fsociaty.com")
+# res.encoding = 'gbk'
+# data = res.text
+# print(data.encode('GBK', 'ignore'))
+
+res = requests.get("https://www.douyu.com/g_DOTA2")
+soup = BeautifulSoup(res.text)
+for item in soup.select('.layout-Cover-item'):
+    print(item.select('.DyListCover-intro')[0].text)
+    print(item.select('.DyListCover-user')[0].text)
+    print(item.select('.DyListCover-hot')[0].text)
