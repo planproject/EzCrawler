6ed26317df868912e9f97076123fad099dc4c46a
FerreWagner <18408229270@163.com>
2019-06-02 23:18:11 +0800

    update

--- url_sg1.py
+++ url_sg1.py
@@ -12,8 +12,15 @@ all_data = soup.select("tbody")
 
 for normal in all_data:
     # soup_title = normal.find(class_="")
-    soup_title = normal.select(".xst")
+    soup_title  = normal.select(".xst")
+    soup_author = normal.select(".by > cite > a")
+    soup_say = normal.select(".num > a")
+    soup_see = normal.select(".num > em")
     if soup_title:
-        print(soup_title[0].text)
+        # print(soup_title[0].text)
+        # print(soup_author[0].text)
+        print(soup_author[1].text)  #最新回复
+        # print(soup_say[0].text)
+        # print(soup_see[0].text)
 
 # print(all_data)
