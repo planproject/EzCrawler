d9a2f0b358e30c16a597fb2f28c3b135fd88f1fe
FerreWagner <18408229270@163.com>
2019-06-10 23:46:06 +0800

    sg-data-crawl

--- url_sg1.py
+++ url_sg1.py
@@ -14,17 +14,19 @@ soup = bs(resp, "html.parser")
 all_data = soup.select("tbody")
 
 #初始化
-# startTime1 = 'SG-' + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-startTime1 = 'asd'
+startTime1 = 'SG-' + str(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime()))
+# startTime1 = 'asd'
 workbook = xlsxwriter.Workbook('D:/'+ startTime1 +'.xlsx')  #创建一个Excel文件
 worksheet = workbook.add_worksheet()               #创建一个sheet
-headings = ['Number','testA','testB']     #设置表头
+headings = ['标题','作者','最后回复','回复数量','浏览量']     #设置表头
 
 title = [U'SG',U'抓取数据']     #表格title
 worksheet.write_row('A1',headings)  #title 写入Excel
 data_title = []
 data_author = []
 data_rec = []
+data_say = []
+data_see = []
 for normal in all_data:
     soup_title  = normal.select(".xst")
     soup_author = normal.select(".by > cite > a")
@@ -34,7 +36,8 @@ for normal in all_data:
         data_title.append(soup_title[0].text)
         data_author.append(soup_author[0].text)
         data_rec.append(soup_author[1].text)
-
+        data_say.append(soup_say[0].text)
+        data_see.append(soup_see[0].text)
         # print(soup_title[0].text) #标题
         # print(soup_author[0].text)    #作者
         # print(soup_author[1].text)  #最新回复
@@ -44,4 +47,6 @@ for normal in all_data:
 worksheet.write_column('A2', data_title)
 worksheet.write_column('B2', data_author)
 worksheet.write_column('C2', data_rec)
+worksheet.write_column('D2', data_say)
+worksheet.write_column('E2', data_see)
 workbook.close()
