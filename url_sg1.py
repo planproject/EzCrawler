9bad72d47e0e26cc3da62e6db58320d6476c8d87
FerreWagner <18408229270@163.com>
2019-06-10 23:38:31 +0800

    insert excel

--- url_sg1.py
+++ url_sg1.py
@@ -22,24 +22,26 @@ headings = ['Number','testA','testB']     #设置表头
 
 title = [U'SG',U'抓取数据']     #表格title
 worksheet.write_row('A1',headings)  #title 写入Excel
+data_title = []
+data_author = []
+data_rec = []
 for normal in all_data:
     soup_title  = normal.select(".xst")
     soup_author = normal.select(".by > cite > a")
     soup_say = normal.select(".num > a")
     soup_see = normal.select(".num > em")
     if soup_title:
-        data_title = soup_title[0].text
-        data_author = soup_author[0].text
-        data_rec = soup_author[1].text
+        data_title.append(soup_title[0].text)
+        data_author.append(soup_author[0].text)
+        data_rec.append(soup_author[1].text)
 
-        worksheet.write_column('A2', data_title)
-        worksheet.write_column('B2', data_author)
-        worksheet.write_column('C2', data_rec)
         # print(soup_title[0].text) #标题
         # print(soup_author[0].text)    #作者
         # print(soup_author[1].text)  #最新回复
         # print(soup_say[0].text)   #回复量
         # print(soup_see[0].text)   #阅读量
 # print(all_data)
-
+worksheet.write_column('A2', data_title)
+worksheet.write_column('B2', data_author)
+worksheet.write_column('C2', data_rec)
 workbook.close()
